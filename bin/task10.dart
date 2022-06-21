2ab716184a311e52bc9e2737766e1b5dedf702f3
G'ayrat <shirnboyevgayrat@gmail.com>
2022-06-21 11:42:36 +0500

    task10

--- bin/task10.dart
+++ bin/task10.dart
@@ -11,7 +11,7 @@
 int func(number) {
   int a = number ~/ 1000;
   int b = number % 1000 ~/ 100;
-  int c = number % 1000 % 100 ~/ 10;
+  int c = number % 100 ~/ 10;
   int f = number % 1000 % 100 % 10;
   return a * b * c * f;
 }
