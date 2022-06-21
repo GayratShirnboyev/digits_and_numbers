b061e0934ed6f08f55d846d9264ce739c5219cd6
G'ayrat <shirnboyevgayrat@gmail.com>
2022-06-21 11:09:26 +0500

    task9

--- bin/task09.dart
+++ bin/task09.dart
@@ -8,4 +8,14 @@
   Returns:
       return answer
   */
-void main() {}
+int func(number) {
+  int a = number ~/ 1000;
+  int y = number % 1000 ~/ 100;
+  int b = number % 1000 % 100 ~/ 10;
+  int c = number % 1000 % 100 % 10;
+  return a + y + b + c;
+}
+
+void main() {
+  print(func(1234));
+}
